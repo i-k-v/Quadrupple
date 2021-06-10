457736dcfb18d20b7b23fe1f3a1de008d88d75cc
i-k-v <83413527+i-k-v@users.noreply.github.com>
2021-06-10 14:59:09 +0300

    .

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const mysql = require('mysql2')
 const path = require('path')
-const connect = mysql.createConnection('mysqlConfig')
+
 
 const mysqlConfig = {
     host: "data_base",
     password: "root12345",
     database: "epicstory"
 }
-
+const connect = mysql.createConnection(mysqlConfig)
 const app = express()
 
 // link nodejs and html page
@@ -17,3 +17,4 @@ app.get('/',function(req,res){
 })
 
 app.listen(3000)
+console.log("Server is UP")
